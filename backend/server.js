64229149b657dcf1e310025f6183e3f9c387c47d
RAHUL <rahulnaval157821@gmail.com>
2024-02-06 22:26:51 +0530

    Revert "first commit"
    This reverts commit ed11146ea352c0bf44c2518a380865a2c749b4c5.

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("hureeeee");
-});
-
-app.listen(5000, console.log("server started on port 5000"));
