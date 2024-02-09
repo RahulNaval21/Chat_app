5ab4a7f62c8e0f24397874c7373f61e06eb24a2e
RAHUL <rahulnaval157821@gmail.com>
2024-02-10 02:14:39 +0530

    Login andSignup UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
-import { HomePage } from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 
 function App() {
