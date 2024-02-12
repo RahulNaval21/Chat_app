f813893efa106bab59e9e4ad0766f7a94f726b00
RAHUL <rahulnaval157821@gmail.com>
2024-02-12 12:44:53 +0530

    signup,login and hashing the password

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,19 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
-const app = express();
 dotenv.config();
 connectDB();
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hureeeee");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  //console.log(req.params.id);
-
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 //const PORT = process.env.PORT || 5000;
 
