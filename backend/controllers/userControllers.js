c41b1cc07900a8355f26e266f55f3e1ecf2604f1
RAHUL <rahulnaval157821@gmail.com>
2024-03-03 08:47:12 +0530

    searchUser

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -57,4 +57,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { regiesterUser, authUser };
+//   /api/user?search=rahul
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+
+  res.send(users);
+});
+
+module.exports = { regiesterUser, authUser, allUsers };
